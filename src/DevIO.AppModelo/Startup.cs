b11ba0ce02d00e7e11874321abfcaf31f1d8fe2a
Guilherme Vada <gvadaguariba@gmail.com>
2021-01-02 20:18:38 -0300

    Implementation minification

--- src/DevIO.AppModelo/Startup.cs
+++ src/DevIO.AppModelo/Startup.cs
@@ -19,6 +19,8 @@ namespace DevIO.AppModelo
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
